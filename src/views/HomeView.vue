16a550a693523a116903b980242cc74ba611ae0a
RAFAELA NASCIMENTO <rafaela.nascimento@websupply.com.br>
2025-09-01 14:48:40 -0300

    Primeiro commit do Training_Vue

---
+++ src/views/HomeView.vue
@@ -0,0 +1,22 @@
+<template>
+  <Banner />
+  <div class="main-container">
+    <h1>Monte o seu burguer:</h1>
+    <BurguerForm />
+  </div>
+</template>
+
+<script>
+import BurguerForm from '@/components/BurguerForm.vue';
+import Banner from '../components/Banner.vue';
+
+export default{
+  name: 'Home',
+  components: {
+    Banner,
+    BurguerForm, 
+  }
+}
+
+
+</script>
